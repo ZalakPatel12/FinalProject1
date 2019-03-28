f912a9356bfaa5b0fb2b637799e981c7f34bf0dc
Angela Arroyo <ara55@njit.edu>
2019-03-28 15:26:56 -0400

    Created test folder with indexTest.php

---
+++ public/test/indexTest.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+final class indexest extends TestCase
+{
+    public function CSV_File() {
+        $this->assertCSV_File('data/03.csv');
+    }
+
+    public function testCSV_File(){
+        echo file_exists('CSV_File');
+    }
+    public function testHTML_Table(){
+        echo file_exists('HTML_Table');
+    }
+    public function testinitialization(){
+        echo file_exists('initialization');
+    }
+
+    public function testindexFile(){
+        echo file_exists('index');
+    }
+
+}
